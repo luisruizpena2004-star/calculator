5fcee61d14fb7796e17fbbc6a7120909a02028ac
Luis Ruiz Peña <luisruizpena2004@gmail.com>
2026-07-10 21:25:59 -0400

    Add basic operate implementation

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+let firstArgument;
+let secondArgument;
+let operator;
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case "add":
+            add(a, b);
+            break;
+        case "substract":
+            substract(a, b);
+            break;
+        case "multiply":
+            multiply(a, b);
+            break;
+        case "divide":
+            divide(a, b);
+            break;
+    }
+}
+
 function add(a, b) {
     return a + b;
 }
